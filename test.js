da76060af4abb78140ae2c869561ff376093b3ae
poke2149 <83256981+poke2149@users.noreply.github.com>
2021-07-08 10:19:46 +0900

    first

---
+++ test.js
@@ -0,0 +1,16 @@
+var a = "비 오는 날";
+var b = "봄 +가을 옷차림";
+
+var a_1 = "비 오는 날";
+var b_1 = "봄 +가을 옷차림";
+
+function str_test(){
+    if(a == a_1){
+        console.log("ok");
+    }
+    if (b == b_1){
+        console.log("b ok");
+    }
+    
+}
+str_test();
